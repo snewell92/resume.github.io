df0a58eabf4324ff53ee5981ecebb24909b1be17
Sean Newell <sean@thenewells.us>
2022-11-05 19:51:40 +0000

    Initial commit
    Got links and administrate in there.
    
    Basic layout, two main components (Job/Card (for a link)) done.

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			keyframes: {
+				shake: {
+					'0%': { transform: 'rotate(0deg)' },
+					'25%': { transform: 'rotate(9deg)' },
+					'50%': { transform: 'rotate(0deg)' },
+					'75%': { transform: 'rotate(-9deg)' },
+					'100%': { transform: 'rotate(0deg)' },
+				}
+			},
+			animation: {
+				'shake-once': 'shake .8s linear 2'
+			}
+		},
+	},
+	plugins: [],
+}
